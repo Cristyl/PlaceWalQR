cdc470029e1ce46a9caf56f9bd4ac1360b32ca63
Keyneees <michele.nicoletti2000@gmail.com>
2025-08-04 23:19:13 +0200

    Homepage incompleted

---
+++ app/src/main/java/com/example/placewalqr/HomepageActivity.kt
@@ -0,0 +1,35 @@
+package com.example.placewalqr
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import org.w3c.dom.Text
+
+class HomepageActivity : ComponentActivity(){
+    lateinit var user: String
+    lateinit var see_sights: Number
+    lateinit var points: Number
+    lateinit var last_place: String
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage_activity)
+        val welcome_label: TextView = findViewById<TextView>(R.id.welcome_label)
+        val welcome_text=welcome_label.text.toString() +" "+ user
+        welcome_label.setText(user)
+
+        val seesights_label: TextView = findViewById<TextView>(R.id.seesights_text)
+        seesights_label.setText(see_sights.toString())
+
+        val points_label: TextView= findViewById<TextView>(R.id.points_text)
+        points_label.setText(points.toString())
+
+        val lastplace_label: TextView= findViewById<TextView>(R.id.lastplace_text)
+        lastplace_label.setText(last_place)
+    }
+
+    fun loadHomePage(){
+        //chiamate api per i dati richeisti
+    }
+}
