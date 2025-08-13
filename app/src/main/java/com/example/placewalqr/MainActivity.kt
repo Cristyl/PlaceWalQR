c7b94413692cddb3141c3ff731c49f50c34d295d
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2025-08-13 14:55:51 +0200

    Update on shared preferences

--- app/src/main/java/com/example/placewalqr/MainActivity.kt
+++ app/src/main/java/com/example/placewalqr/MainActivity.kt
@@ -29,11 +29,8 @@ class MainActivity : ComponentActivity() {
     private lateinit var emailField: TextView
     private lateinit var pwdField: TextView
     private lateinit var loginBtn: Button
-    private lateinit var cameraBtn: Button
     private lateinit var forgotPwd: TextView
     private lateinit var registerText: TextView
-    private lateinit var homepageBtn: Button
-    private lateinit var mapBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,12 +42,13 @@ class MainActivity : ComponentActivity() {
         forgotPwd = findViewById(R.id.forgot_pwd_text)
         registerText = findViewById(R.id.register_text)
         loginBtn = findViewById(R.id.btnLogin)
-//        cameraBtn = findViewById(R.id.camera_btn)
-//        homepageBtn=findViewById(R.id.homepage_btn)
-//        mapBtn=findViewById(R.id.map_btn)
+
+
+        // cancello tutti i dati usati nella sessione precedente
+        getSharedPreferences("UserPrefs", MODE_PRIVATE).edit().clear().apply()
 
         forgotPwd.setOnClickListener {
-            //
+            //TODO
         }
 
         loginBtn.setOnClickListener {
@@ -61,21 +59,6 @@ class MainActivity : ComponentActivity() {
             var intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
-//        cameraBtn.setOnClickListener {
-//            var intent = Intent(this, CameraActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        homepageBtn.setOnClickListener {
-//            var intent = Intent(this, HomepageActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        mapBtn.setOnClickListener {
-//            var intent = Intent(this, MapsActivity::class.java)
-//            startActivity(intent)
-//        }
 
     }
 
